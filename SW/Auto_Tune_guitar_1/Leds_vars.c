72f239d8f67668ef21afad76a9671c05a7fdb01a
recrutty <recrutty@gmail.com>
2017-02-27 11:23:09 +0100

    Update Leds_vars.c

--- SW/Auto_Tune_guitar_1/Leds_vars.c
+++ SW/Auto_Tune_guitar_1/Leds_vars.c
@@ -1,24 +1,23 @@
+#include "GPIO_binding.h"
+#include <stdint.h>
+#include "xc.h"
+#include "Leds.h"
 
-const StepMotorLed Led_Motor1 = {STEP1_LED_PORT, STEP1_LED_DIR, STEP1_LED_ANSEL, STEP1_LED_BIT};
-const StepMotorLed Led_Motor2 = {STEP2_LED_PORT, STEP2_LED_DIR, STEP2_LED_ANSEL, STEP2_LED_BIT};
-const StepMotorLed Led_Motor3 = {STEP3_LED_PORT, STEP3_LED_DIR, STEP3_LED_ANSEL, STEP3_LED_BIT};
-const StepMotorLed Led_Motor4 = {STEP4_LED_PORT, STEP4_LED_DIR, STEP4_LED_ANSEL, STEP4_LED_BIT};
-const StepMotorLed Led_Motor5 = {STEP5_LED_PORT, STEP5_LED_DIR, STEP5_LED_ANSEL, STEP5_LED_BIT};
-const StepMotorLed Led_Motor6 = {STEP6_LED_PORT, STEP6_LED_DIR, STEP6_LED_ANSEL, STEP6_LED_BIT};
-const StepMotorLed Led_Motor7 = {STEP7_LED_PORT, STEP7_LED_DIR, STEP7_LED_ANSEL, STEP7_LED_BIT}; 
+#undef LED
+#define LED(_name_, _po_, _tr_, _an_, _pin_)          \
+    Led _name_ =                        \
+    {                                   \
+        .Port = _po_,                   \        
+        .Tris = _tr_,                   \
+        .Ansel = _an_,                  \
+        .LedPin = _pin_                 \
+    };
 
+#include "Led_list.h"
 
-        /*
-        StepMotorLed *xled = (StepMotorLed*)(StepperMotorList[i]->Led);
-        *(xled->Tris) &= ~(1 << xled->LedPin);
-        *(xled->Ansel) &= ~(1 << xled->LedPin);
-*/
-/*
-typedef struct
-{
-    volatile uint16_t *Port;
-    volatile uint16_t *Tris;
-    volatile uint16_t *Ansel;
-    volatile uint8_t LedPin;
-} StepMotorLed;
-*/
+#undef LED
+#define LED(_name_, _po_, _tr_, _an_, _pin_)   &_name_,
+
+Led *LedList[] ={
+#include "StepperMotor_list.h"
+};
