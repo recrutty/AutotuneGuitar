bba56dc48e95bfd2adcf60e3b62076b794df70f0
recrutty <recrutty@gmail.com>
2017-02-27 12:38:38 +0100

    Update Leds.c

--- SW/Auto_Tune_guitar_1/Leds.c
+++ SW/Auto_Tune_guitar_1/Leds.c
@@ -1,1 +1,16 @@
-karel
+#include "GPIO_binding.h"
+#include <stdint.h>
+#include "xc.h"
+#include "Leds.h"
+
+extern Led *LedList[];
+
+void InitLeds()
+{
+    int i;
+    for (i = 0; i < eLedNum; i++) 
+    {
+        *((LedList[i])->Tris) &= ~(1 << ((LedList[i])->LedPin));
+        *((LedList[i])->Ansel) &= ~(1 << ((LedList[i])->LedPin));
+    }
+}
