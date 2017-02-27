967e98a6863d885613ceb9edb871c08ce2a3c4e9
recrutty <recrutty@gmail.com>
2017-02-27 10:13:27 +0100

    Update Leds.h

--- SW/Auto_Tune_guitar_1/Leds.h
+++ SW/Auto_Tune_guitar_1/Leds.h
@@ -1,1 +1,30 @@
-karel
+#ifndef LEDS_H
+#define	LEDS_H
+#include <stdint.h>
+#include "xc.h"
+#include "GPIO_binding.h"
+
+typedef struct
+{
+    volatile uint16_t *Port;
+    volatile uint16_t *Tris;
+    volatile uint16_t *Ansel;
+    volatile uint8_t LedPin;
+} Led;
+
+#undef LED
+#define LED (_name_, _po_, _tr_, _an_, _pin_)   e##_name_,
+
+typedef enum
+{
+    #include "Led_list.h"
+    eLedNum
+} LedIndex;
+
+void InitLeds();
+void TurnLedOn(uint8_t ledNum);
+void TurnLedOff(uint8_t ledNum);
+void OneLedOn(uint8_t ledNum);
+void AllLedsOff();
+
+#endif	/* LEDS_H */
