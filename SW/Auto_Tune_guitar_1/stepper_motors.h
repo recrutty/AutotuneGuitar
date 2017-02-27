cf4b9974f369971cbd772321d8ec0d155e092908
recrutty <recrutty@gmail.com>
2017-02-27 09:53:19 +0100

    deleting leds

--- SW/Auto_Tune_guitar_1/stepper_motors.h
+++ SW/Auto_Tune_guitar_1/stepper_motors.h
@@ -12,14 +12,6 @@ typedef struct
     volatile uint8_t FirstPin;
 } StepMotorCoil;
 
-typedef struct
-{
-    volatile uint16_t *Port;
-    volatile uint16_t *Tris;
-    volatile uint16_t *Ansel;
-    volatile uint8_t LedPin;
-} StepMotorLed;
-
 typedef struct{
     uint16_t RemainingSteps;
     uint8_t UpDown:1;
@@ -31,20 +23,17 @@ typedef struct
 {
     const StepMotorCoil *CoilA;
     const StepMotorCoil *CoilB;
-    const StepMotorLed *Led;    
     volatile StepMotorState LastState;
 } StepMotor;
 
 #undef STEPPER_MOTOR
-#define STEPPER_MOTOR(_name_, _cA_, _cB_, _led_)   e##_name_,
+#define STEPPER_MOTOR(_name_, _cA_, _cB_)   e##_name_,
 
 typedef enum
 {
     #include "StepperMotor_list.h"
     eStepperMotorNum
 } StepMotorIndex;
-
-
 
 void InitMotorsAndLeds();
 void DoStep(uint8_t motorNum);
