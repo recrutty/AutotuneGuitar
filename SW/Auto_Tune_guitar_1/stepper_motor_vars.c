e02121caf2d8d0a6a6208a9f78805d9ac795e874
recrutty <recrutty@gmail.com>
2017-02-27 09:49:23 +0100

    deleting leds

--- SW/Auto_Tune_guitar_1/stepper_motor_vars.c
+++ SW/Auto_Tune_guitar_1/stepper_motor_vars.c
@@ -18,30 +18,21 @@ const StepMotorCoil CoilB_Motor5 = {STEP5_B_PORT, STEP5_B_DIR, STEP5_B_ANSEL, ST
 const StepMotorCoil CoilA_Motor6 = {STEP6_A_PORT, STEP6_A_DIR, STEP6_A_ANSEL, STEP6_A_BIT};
 const StepMotorCoil CoilB_Motor6 = {STEP6_B_PORT, STEP6_B_DIR, STEP6_B_ANSEL, STEP6_B_BIT};
 const StepMotorCoil CoilA_Motor7 = {STEP7_A_PORT, STEP7_A_DIR, STEP7_A_ANSEL, STEP7_A_BIT};
-const StepMotorCoil CoilB_Motor7 = {STEP7_B_PORT, STEP7_B_DIR, STEP7_B_ANSEL, STEP7_B_BIT};
-
-const StepMotorLed Led_Motor1 = {STEP1_LED_PORT, STEP1_LED_DIR, STEP1_LED_ANSEL, STEP1_LED_BIT};
-const StepMotorLed Led_Motor2 = {STEP2_LED_PORT, STEP2_LED_DIR, STEP2_LED_ANSEL, STEP2_LED_BIT};
-const StepMotorLed Led_Motor3 = {STEP3_LED_PORT, STEP3_LED_DIR, STEP3_LED_ANSEL, STEP3_LED_BIT};
-const StepMotorLed Led_Motor4 = {STEP4_LED_PORT, STEP4_LED_DIR, STEP4_LED_ANSEL, STEP4_LED_BIT};
-const StepMotorLed Led_Motor5 = {STEP5_LED_PORT, STEP5_LED_DIR, STEP5_LED_ANSEL, STEP5_LED_BIT};
-const StepMotorLed Led_Motor6 = {STEP6_LED_PORT, STEP6_LED_DIR, STEP6_LED_ANSEL, STEP6_LED_BIT};
-const StepMotorLed Led_Motor7 = {STEP7_LED_PORT, STEP7_LED_DIR, STEP7_LED_ANSEL, STEP7_LED_BIT}; // </editor-fold>
+const StepMotorCoil CoilB_Motor7 = {STEP7_B_PORT, STEP7_B_DIR, STEP7_B_ANSEL, STEP7_B_BIT};// </editor-fold>
 
 // <editor-fold defaultstate="collapsed" desc="Motors array declaration">
 #undef STEPPER_MOTOR
-#define STEPPER_MOTOR(_name_, _cA_, _cB_, _led_)   \
+#define STEPPER_MOTOR(_name_, _cA_, _cB_)   \
     StepMotor _name_ =                  \
     {                                   \
         .CoilA = &_cA_,                 \
-        .CoilB = &_cB_,                 \
-        .Led = &_led_                   \
+        .CoilB = &_cB_
     };
 
 #include "StepperMotor_list.h"
 
 #undef STEPPER_MOTOR
-#define STEPPER_MOTOR(_name_, _cA_, _cB_, _led_)   &_name_,
+#define STEPPER_MOTOR(_name_, _cA_, _cB_)   &_name_,
 
 StepMotor *StepperMotorList[] ={
 #include "StepperMotor_list.h"
