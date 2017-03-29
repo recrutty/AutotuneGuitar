37b1b48c4019bbd3f9dbc76dfaf988612e6ca1a7
recrutty <recrutty@gmail.com>
2017-03-29 13:49:23 +0200

    zacatek vyhodnoceni fft

--- SW/Auto_Tune_guitar_1/fft.c
+++ SW/Auto_Tune_guitar_1/fft.c
@@ -19,33 +19,26 @@ void InitFFT()
 void CalculateFFT(int inputValues[], fractional outputValues[FFT_BLOCK_LENGTH/2])
 {
     int i;
-    for ( i = 0; i < FFT_BLOCK_LENGTH; i++ )/* The FFT function requires input data */
-    {					/* to be in the fractional fixed-point range [-0.5, +0.5]*/
-            sigCmpx[i].real = (inputValues[i] - ADC_MIDDLE) ;		/* So, we shift all data samples by 1 bit to the right. */
-            sigCmpx[i].imag = 0;                    /* Should you desire to optimize this process, perform  */
-            if (sigCmpx[i].real<(-128)) 
-            {
-                sigCmpx[i].real = -32768;
-            }
-            else if (sigCmpx[i].real>(128)) 
-            {
-                sigCmpx[i].real = 32767;
-            }
-            else
-            {                
-                sigCmpx[i].real *= 128;
-            }
-
-
+    for ( i = 0; i < FFT_BLOCK_LENGTH; i++ )
+    {				
+        sigCmpx[i].real = (inputValues[i] - ADC_MIDDLE);		
+        sigCmpx[i].imag = 0;
+        if (sigCmpx[i].real<(-128)) 
+        {
+            sigCmpx[i].real = INT16_MIN;
+        }
+        else if (sigCmpx[i].real>(128)) 
+        {
+            sigCmpx[i].real = INT16_MAX;
+        }
+        else
+        {
+            sigCmpx[i].real *= ADC_MULTIPLIER_IDEAL;
+        }
     }
+    
     FFTComplexIP(LOG2_BLOCK_LENGTH, &sigCmpx[0], &twiddleFactors[0], COEFFS_IN_DATA);
     BitReverseComplex (LOG2_BLOCK_LENGTH, &sigCmpx[0]);
-    //sigCmpx[0].real = 0 ;
-    /*for ( i = 0; i < FFT_BLOCK_LENGTH; i++ ) The FFT function requires input data
-    {					
-            sigCmpx[i].real = sigCmpx[i].real << 4 ;		
-            sigCmpx[i].imag = sigCmpx[i].imag << 4;                 
-    }*/
     SquareMagnitudeCplx(FFT_BLOCK_LENGTH/2, &sigCmpx[0], outputValues);
     
 }
--- SW/Auto_Tune_guitar_1/fft.h
+++ SW/Auto_Tune_guitar_1/fft.h
@@ -2,11 +2,12 @@
 #define	FFT_H
 #include <dsp.h>
 #define ADC_MIDDLE (2043)
-#define LOG2_BLOCK_LENGTH     (11)    /* = Number of "Butterfly" Stages in FFT processing */
-#define FFT_BLOCK_LENGTH   ((1) << (LOG2_BLOCK_LENGTH)) // 512     /* = Number of frequency points in the FFT */
-#define SAMPLING_RATE        (10000)    /* = Rate at which input signal was sampled */
-                                        /* SAMPLING_RATE is used to calculate the frequency*/
-                                        /* of the largest element in the FFT output vector*/
+#define ADC_MULTIPLIER_IDEAL_BV (7)
+#define ADC_MULTIPLIER_IDEAL (1 << (ADC_MULTIPLIER_IDEAL_BV))
+#define LOG2_BLOCK_LENGTH     (11)                                  
+#define FFT_BLOCK_LENGTH   ((1) << (LOG2_BLOCK_LENGTH))
+#define SAMPLING_RATE        (10000)  
+
 void InitFFT();
 void CalculateFFT(int inputValues[FFT_BLOCK_LENGTH], fractional outputValues[FFT_BLOCK_LENGTH/2]);
 #endif	/* FFT_H */
